933ea5396efd4a2424752cebc988be766fe921f9
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2021-08-30 19:18:14 -0400

    refac: pokemon type icon refac code

--- app/src/main/java/com/khaleb/pokedex/view/PokedexAdapter.kt
+++ app/src/main/java/com/khaleb/pokedex/view/PokedexAdapter.kt
@@ -13,12 +13,12 @@ import com.khaleb.pokedex.databinding.LayoutIconRecyclerBinding
 import com.khaleb.pokedex.model.Pokemon
 
 class PokedexAdapter(
-    var context : Context
-)  : RecyclerView.Adapter<PokedexAdapter.ViewHolder>() {
+    var context: Context
+) : RecyclerView.Adapter<PokedexAdapter.ViewHolder>() {
 
     var teste = ArrayList<Pokemon>()
 
-    class ViewHolder(view: LayoutIconRecyclerBinding) : RecyclerView.ViewHolder(view.root){
+    class ViewHolder(view: LayoutIconRecyclerBinding) : RecyclerView.ViewHolder(view.root) {
         val binding = LayoutIconRecyclerBinding.bind(view.root)
 
     }
@@ -29,6 +29,11 @@ class PokedexAdapter(
     }
 
     override fun onBindViewHolder(holder: PokedexAdapter.ViewHolder, position: Int) {
+
+        holder.binding.imgTypeFire.visibility = View.GONE
+        holder.binding.imgTypeNormal.visibility = View.GONE
+        holder.binding.imgTypeWater.visibility = View.GONE
+
         holder.binding.txtName.text = teste[position].name
         holder.binding.txtId.text = teste[position].id
 
@@ -36,10 +41,19 @@ class PokedexAdapter(
             .load(teste[position].imageurl)
             .into(holder.binding.imgPokemon)
 
-        when(teste[position].typeofpokemon.size){
-            1 -> checkPokemonType(holder.binding.imgTypeFire, teste[position].typeofpokemon[0])
+        when (teste[position].typeofpokemon.size) {
+            1 -> checkPokemonType(holder.binding.imgTypeWater, teste[position].typeofpokemon[0])
+            2 -> {
+                checkPokemonType(holder.binding.imgTypeWater, teste[position].typeofpokemon[0])
+                checkPokemonType(holder.binding.imgTypeFire, teste[position].typeofpokemon[1])
+            }
+            3 -> {
+                checkPokemonType(holder.binding.imgTypeWater, teste[position].typeofpokemon[0])
+                checkPokemonType(holder.binding.imgTypeFire, teste[position].typeofpokemon[1])
+                checkPokemonType(holder.binding.imgTypeNormal, teste[position].typeofpokemon[2])
+            }
+            else -> {}
         }
-
 
 
     }
@@ -48,18 +62,63 @@ class PokedexAdapter(
         return teste.size ?: 0
     }
 
-    fun update(pokeList : List<Pokemon>){
+    fun update(pokeList: List<Pokemon>) {
         teste.addAll(pokeList)
         notifyDataSetChanged()
     }
 
 
-    private fun checkPokemonType(view : ImageView, type : String ){
-        when(type){
-            "Fire" ->{
+    private fun checkPokemonType(view: ImageView, type: String) {
+        when (type) {
+            "Fire" -> {
+                view.setImageResource(R.drawable.ic_type_fire)
+                view.visibility = View.VISIBLE
+            }
+            "Water" -> {
+                view.setImageResource(R.drawable.ic_type_water)
+                view.visibility = View.VISIBLE
+            }
+            "Grass" -> {
+                view.setImageResource(R.drawable.ic_type_grass)
+                view.visibility = View.VISIBLE
+            }
+            "Psychic" -> {
+                view.setImageResource(R.drawable.ic_type_psychic)
+                view.visibility = View.VISIBLE
+            }
+            "Poison" -> {
+                view.setImageResource(R.drawable.ic_type_poison)
+                view.visibility = View.VISIBLE
+            }
+            "Flying" -> {
                 view.setImageResource(R.drawable.ic_type_flying)
                 view.visibility = View.VISIBLE
             }
+            "Rock" -> {
+                view.setImageResource(R.drawable.ic_type_rock)
+                view.visibility = View.VISIBLE
+            }
+            "Fighting" -> {
+                view.setImageResource(R.drawable.ic_type_fighting)
+                view.visibility = View.VISIBLE
+            }
+            "Electric" -> {
+                view.setImageResource(R.drawable.ic_type_electric)
+                view.visibility = View.VISIBLE
+            }
+            "Ground" -> {
+                view.setImageResource(R.drawable.ic_type_ground)
+                view.visibility = View.VISIBLE
+            }
+            "Steel" -> {
+                view.setImageResource(R.drawable.ic_type_steel)
+                view.visibility = View.VISIBLE
+            }
+            "Normal" -> {
+                view.setImageResource(R.drawable.ic_type_normal)
+                view.visibility = View.VISIBLE
+            }
+            else -> view.visibility = View.GONE
         }
     }
 }
