773815d264eb46a2dd22ecb73fc222c7f831d561
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2021-09-07 13:27:08 -0400

    add: network interceptor

--- app/src/main/java/com/khaleb/pokedex/model/network/PokemonAPI.kt
+++ app/src/main/java/com/khaleb/pokedex/model/network/PokemonAPI.kt
@@ -1,6 +1,8 @@
 package com.khaleb.pokedex.model.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,11 +12,19 @@ class PokemonAPI() {
 
     @JvmName("getInstance1")
     private fun getInstance(): Retrofit {
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         if (instance == null) {
             instance = Retrofit.Builder()
                 .baseUrl("https://gist.githubusercontent.com/mrcsxsiq/b94dbe9ab67147b642baa9109ce16e44/raw/97811a5df2df7304b5bc4fbb9ee018a0339b8a38/")
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(OkHttpClient())
+                .client(client)
                 .build()
         }
         return instance!!
